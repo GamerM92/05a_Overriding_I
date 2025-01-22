6787fc8696f011cd977ce99a079bfbdf32de4efc
GamerM92 <manuel_marx@outlook.de>
2025-01-22 12:08:01 +0100

    GrandParent

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,15 +4,18 @@ public class App {
     
     public static void main(String[] args) {
      
+       Child child = new Child();
        
-       
+        output(child.sayHello());
+        output(child.sayHello("Hi from "));
+
 
     }
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
+    
 
 
 }
